a0674cfe8d79ffb968754f5293d22c9fbcf3e9cf
Ignacio Romero <iromero@dzen.cl>
2013-02-27 13:51:27 -0300

    Added rounded corners to the view container

--- Source/UIViewController+MJPopupViewController.m
+++ Source/UIViewController+MJPopupViewController.m
@@ -103,6 +103,7 @@ static void * const keypath = (void*)&keypath;
     popupView.layer.shadowOffset = CGSizeMake(5, 5);
     popupView.layer.shadowRadius = 5;
     popupView.layer.shadowOpacity = 0.5;
+    popupView.layer.cornerRadius = 6.0f;
     
     // Add semi overlay
     UIView *overlayView = [[UIView alloc] initWithFrame:sourceView.bounds];
